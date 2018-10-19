4213f70d582bb20a5e933de530567295559ebe89
TimG <timgm3@gmail.com>
2018-10-18 19:04:56 -0700

    multiple notes can be pulled

--- public/app.js
+++ public/app.js
@@ -35,10 +35,18 @@ $(document).on("click", "p", function () {
       $("#notes").append("<button data-id='" + data._id + "' id='deletenote'>Delete Note</button>");
       // If there's a note in the article
       if (data.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
+        console.log(data.note);
+        for (let i = 0; i < data.note.length; i++) {
+          $("#notes").append("<h2>" + data.title + "</h2>");
+          // An input to enter a new title
+          $("#notes").append("<input id='titleinput' value='" + data.note[i].title + "' name='title' >");
+          // A textarea to add a new note body
+          $("#notes").append("<textarea id='bodyinput name='body' >" + data.note[i].body + "</textarea>");
+          // A button to submit a new note, with the id of the article saved to it
+          $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+          $("#notes").append("<button data-id='" + data._id + "' id='deletenote'>Delete Note</button>");
+        };
+
       }
     })
     .catch(function (err) {
